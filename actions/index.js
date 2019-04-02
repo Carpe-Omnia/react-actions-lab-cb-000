b0817b2a12b19cfbc142758b6bcea84fe39c0dcd
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-02 23:18:14 +0000

    Automatically backed up by Learn

--- actions/index.js
+++ actions/index.js
@@ -47,9 +47,7 @@ function changeCell (rowIndex, columnIndex, ev) {
 
 function focusCell (rowIndex, columnIndex) {
   console.log(`${rowIndex} - ${columnIndex}`)
-  this.setState({
-    focused: `${rowIndex} - ${columnIndex}`
-  })
+  document.getElementByClass('spreadsheet_focused').innerHTML(`${rowIndex} - ${columnIndex}`)
 }
 
 function blurCell () {
