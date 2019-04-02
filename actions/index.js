fab2ed226ca4dd79cd1aed3665323c676aaba99f
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-02 23:02:19 +0000

    Automatically backed up by Learn

--- actions/index.js
+++ actions/index.js
@@ -33,6 +33,7 @@ function addRow (ev) {
 function changeCell (rowIndex, columnIndex, ev) {
   console.log(ev) ;
   console.log(ev.charCode) ;
+  console.log(ev.target.value)
   console.log(rowIndex) ;
   console.log(columnIndex) ;
   var newTable = this.state.table ;
