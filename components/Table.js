e09a00af365b5876c0c1a8b9258e7d57a4848a54
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-02 21:26:38 +0000

    Automatically backed up by Learn

--- components/Table.js
+++ components/Table.js
@@ -9,7 +9,7 @@ export default class Table extends React.Component {
       <table className='table' onBlur={this.props.onBlur}>
         <tbody>
           this.props.cells.map(function(row, index) =>
-            <Row key={index} row={row} onChange={this.props.onChange.bind(null, rowIndex)} onFocus={this.props.onFocus.bind(null, index)} />
+            <Row  row={row} onChange={this.props.onChange.bind(null, rowIndex)} onFocus={this.props.onFocus.bind(null, index)} />
           )
         </tbody>
       </table>
