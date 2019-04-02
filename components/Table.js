4c24cdb6cb67040940341ec7af3a587af2cab10c
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-02 21:53:29 +0000

    Automatically backed up by Learn

--- components/Table.js
+++ components/Table.js
@@ -9,7 +9,7 @@ const Table = (props) => {
       this.props.cells.map(function(row){
         <Row row={row} onChange={this.props.onChange} onFocus={this.props.onFocus} />
       }, this
-    )  
+    )
   }
   return (
     <table className='table' onBlur={this.props.onBlur}>
