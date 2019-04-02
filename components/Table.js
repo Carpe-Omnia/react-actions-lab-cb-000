e7c32b92c9f1445493c38f54bf371d04214f25c7
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-02 21:56:14 +0000

    Automatically backed up by Learn

--- components/Table.js
+++ components/Table.js
@@ -4,11 +4,7 @@ import React from 'react';
 import Row from './Row';
 
 const Table = (props) => {
-  var rows = {
-      props.cells.map(function(row){
-        <Row row={row} onChange={.props.onChange} onFocus={props.onFocus} />
-      })
-  }
+  var rows = props.cells.map(function(row){ return (<Row row={row} onChange={.props.onChange} onFocus={props.onFocus} />) })
   return (
     <table className='table' onBlur={props.onBlur}>
       <tbody>
