f189484a3d19fed18b82c748b6162b3777a639b6
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-02 23:31:14 +0000

    Automatically backed up by Learn

--- components/Spreadsheet.js
+++ components/Spreadsheet.js
@@ -38,11 +38,6 @@ export default class Spreadsheet extends React.Component {
         <Table cells={this.state.table} doBlur={this.handleBlur} doFocus={this.handleFocus} doChange={this.handleChange} />
 
         <div className='spreadsheet__focused' id="focused">
-        {
-          this.state.focused
-          ? {this.state.focused}
-          : null
-        }
         </div>
       </div>
     );
