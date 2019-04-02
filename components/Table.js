4763957d344e13438cf99393506079671a144539
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-02 21:47:54 +0000

    Automatically backed up by Learn

--- components/Table.js
+++ components/Table.js
@@ -4,8 +4,11 @@ import React from 'react';
 import Row from './Row';
 
 export default class Table extends React.Component {
-  var rows = this.props.cells.map((row) =>{
-      <Row row={row} onChange={this.props.onChange} onFocus={this.props.onFocus} />
+  constructor(props){
+    super(props);
+  }
+  rows = this.props.cells.map((row) =>{
+    <Row row={row} onChange={this.props.onChange} onFocus={this.props.onFocus} />
   }
   render () {
     return (
